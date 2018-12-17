d3dc48b33c56076dab57d558efa4e25a261e5dc3
leo <leo.sl.liu1992@163.com>
2018-12-17 20:00:15 +0800

    完善xml工具类

--- src/main/java/com/leo/util/xml/XmlTools.java
+++ src/main/java/com/leo/util/xml/XmlTools.java
@@ -1,7 +1,10 @@
 package com.leo.util.xml;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
@@ -16,6 +19,13 @@ import java.io.*;
 public class XmlTools {
     private static JAXBContext jaxbContext;
 
+    /**
+     * 通过xml文件地址将xml文件解析为bean
+     *
+     * @param xmlPath 文件地址
+     * @param clazz   类型
+     * @return T
+     */
     public static <T> T readXmlToBean(String xmlPath, Class<T> clazz) {
         StringBuffer buffer = null;
         try {
@@ -34,6 +44,13 @@ public class XmlTools {
         return null;
     }
 
+    /**
+     * 通过xml字符串将xml解析为bean
+     *
+     * @param xmlData xml数据
+     * @param clazz   类型
+     * @return T
+     */
     public static <T> T readXmlDataToBean(String xmlData, Class<T> clazz) {
         try {
             //加载映射bean类
@@ -41,11 +58,28 @@ public class XmlTools {
             //创建解析
             Unmarshaller um = jaxbContext.createUnmarshaller();
             StreamSource streamSource = new StreamSource(new StringReader(xmlData));
-            T root = (T)um.unmarshal(streamSource);
-            return root;
+            return (T) um.unmarshal(streamSource);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String parseBeanToXml(Object data) {
+        try {
+            StringWriter sw = new StringWriter();
+            jaxbContext = JAXBContext.newInstance(data.getClass());
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            if (marshaller == null) {
+                return StringUtils.EMPTY;
+            }
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            marshaller.marshal(data, sw);
+            return sw.toString();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            throw new RuntimeException("obj toXML failed", e);
+        }
+
     }
 }
